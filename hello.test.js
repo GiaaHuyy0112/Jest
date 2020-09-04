7c26ada548f4fb73d08ee2898e18e214a8e9ab9a
Nguyen Tran Gia Huy <huylaga159@gmail.com>
2020-09-04 14:24:02 +0700

    Update 2
    Update total 3 testcases

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,19 @@
 const say = require('./hello');
 const sad = require('./hello');
+
+
+
 test('Hello equal Hello', () => {
-	var s = sad();
+	var s = say();
   expect( s ).toMatch(':((');
+});
+
+test('Emotion', () =>{
+  var e = sad();
+  expect(e).toMatch(':((');
+});
+
+test("Wrong emotion", () => {
+  var e = sad();
+  expect(e).toMatch('xD');
 });
